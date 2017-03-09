05986d8cea7f8344cd910324f9931e428986656e
Seblerr <sibbe-95@hotmail.com>
2017-03-09 15:28:13 +0100

    Updated bookings table mysql

--- database.sql
+++ database.sql
@@ -15,22 +15,21 @@ CREATE TABLE `members` (
   `password` char(128) NOT NULL,
   `map` varchar(65000) NOT NULL,
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `tables` (
   `tableID` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   KEY `email_idx` (`email`),
   CONSTRAINT `email` FOREIGN KEY (`email`) REFERENCES `members` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `bookings` (
-  `hour` int(11) NOT NULL,
-  `tableID` varchar(45) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `duration` int(11) NOT NULL,
-  `minute` int(11) NOT NULL,
+  `tableID` int(11) NOT NULL,
+  `time` time NOT NULL,
+  `duration` int(11) NOT NULL DEFAULT '7200',
   KEY `bookingemail_idx` (`email`),
   CONSTRAINT `bookingemail` FOREIGN KEY (`email`) REFERENCES `tables` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
